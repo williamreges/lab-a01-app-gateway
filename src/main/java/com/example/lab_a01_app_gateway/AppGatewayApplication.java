bd54328508a58e77e0e4c63abce4edb744779b56
william-reges <william.reges1986@gmail.com>
2025-02-06 13:22:47 -0300

    update app

---
+++ src/main/java/com/example/lab_a01_app_gateway/AppGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_a01_app_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab_a01_app_gateway/AppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_a01_app_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
