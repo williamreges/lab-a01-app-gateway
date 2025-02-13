b6c43f3ca0dd2403717bb94dcbb0ef0146467f7d
william-reges <william.reges1986@gmail.com>
2025-02-13 15:53:09 -0300

    update version v2 to v3

--- dockerfile-build.sh
+++ dockerfile-build.sh
@@ -1,7 +1,9 @@
 NAME_IMAGE=lab-a01-app-gateway
+NAME_REPO=williamreges
 
 ./mvnw clean install -Dtest.Skip=true
 
 echo "=== Build Image Version ===="
-docker build -t ${NAME_IMAGE} .
+docker build -t ${NAME_REPO}/${NAME_IMAGE} .
+
 
--- dockerfile-execution.sh
+++ dockerfile-execution.sh
@@ -1,9 +1,9 @@
 NAME_IMAGE=lab-a01-app-gateway
-
+NAME_REPO=williamreges
 
 echo "Docker RUN Local Network HOST"
 docker run -p 8761:8761 \
 --net=host \
---name ${NAME_IMAGE} \
---rm ${NAME_IMAGE}
+--name ${NAME_REPO}/${NAME_IMAGE} \
+--rm ${NAME_REPO}/${NAME_IMAGE}
 
